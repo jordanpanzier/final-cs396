b7c42932946e8ba9efb24cf0b0fe09de6d9692d8
Jordan Panzier <jordanpanzier@jordans-mbp.attlocal.net>
2021-05-19 21:27:26 -0500

    update

---
+++ config/public/bot.js
@@ -0,0 +1,38 @@
+letterboxd("jpanzier")
+        .then((items) => 
+            
+            items.forEach((item) => {
+                if (item.type != 'list' && item.date.watched != 1609372800000){
+                    const movie = {
+                        title:item.film.title,
+                        rating:item.rating.score
+                    }
+                    fetch(`${baseURL}/movies/`)
+                    .then(response => response.json())
+                    .then(data => {
+                        fetch(`${baseURL}/movies/`, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(movie)
+                        })
+                        .then(response => {
+                            if (!response.ok) {
+                                throw Error(response.statusText);
+                            } else {
+                                return response.json();
+                            }
+                        })
+                        .then(data => {
+                            console.log('Success:', data);
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            alert('Error!');
+                        });
+                    })
+                }
+            }))
+            
+        .catch((error) => console.log(error));
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const baseURL = "http://localhost:8081/";
+const baseURL = "https://letterboxd-jp.herokuapp.com/";
 const fetch = require("node-fetch");
 
 require("dotenv").config();
@@ -46,45 +46,6 @@ mongoose.connect(config.database, config.mongoConfig, err => {
         console.log(err);
     } else {
         console.log(`Connected to ${process.env.DB_NAME}.`);
-        letterboxd("jpanzier")
-            .then((items) => 
-                
-                items.forEach((item) => {
-                    if (item.type != 'list' && item.date.watched != 1609372800000){
-                        const movie = {
-                            title:item.film.title,
-                            rating:item.rating.score
-                        }
-                        fetch(`${baseURL}/movies/`)
-                        .then(response => response.json())
-                        .then(data => {
-                            fetch(`${baseURL}/movies/`, {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/json'
-                                },
-                                body: JSON.stringify(movie)
-                            })
-                            .then(response => {
-                                if (!response.ok) {
-                                    throw Error(response.statusText);
-                                } else {
-                                    return response.json();
-                                }
-                            })
-                            .then(data => {
-                                console.log('Success:', data);
-                            })
-                            .catch(err => {
-                                console.error(err);
-                                alert('Error!');
-                            });
-                        })
-                    }
-                }))
-                
-            .catch((error) => console.log(error));
-
     }
 });
 
